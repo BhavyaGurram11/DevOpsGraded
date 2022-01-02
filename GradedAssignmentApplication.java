9758b56bd9a6407b7a2f659bf5b5f38c93b49c45
BhavyaGurram11 <96463512+BhavyaGurram11@users.noreply.github.com>
2022-01-02 16:01:57 +0530

    Update GradedAssignmentApplication.java

--- GradedAssignmentApplication.java
+++ GradedAssignmentApplication.java
@@ -1,9 +1,4 @@
 
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class GradedAssignmentApplication {
 
 	public static void main(String[] args) {
